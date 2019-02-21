6a32c282f24672956bc8ad2f2b89de69a014e5a5
loenz <loenzpy@gmail.com>
2019-02-21 23:46:55 +0400

    Создан конструктор класса Person

--- HR_CMS/src/profile/Person.java
+++ HR_CMS/src/profile/Person.java
@@ -8,5 +8,11 @@ public class Person {
     private Date birthDate;
     private Gender gender;
 
+    public Person(String firstName, String lastName, Date birthDate, Gender gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.gender = gender;
+    }
 
 }
