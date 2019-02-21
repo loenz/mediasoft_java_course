5f779c873f453f442935639fb9102bacb3ab9561
loenz <loenzpy@gmail.com>
2019-02-21 23:15:27 +0400

    Создан класс Person.java. Созданы переменные "имя", "фамилия", "дата рождения", "пол" типа Gender перечислением

--- HR_CMS/src/profile/Person.java
+++ HR_CMS/src/profile/Person.java
@@ -1,4 +1,12 @@
 package profile;
 
+import java.util.Date;
+
 public class Person {
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+    private Gender gender;
+
+
 }
